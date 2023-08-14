9bf246c7a7a248ca81a37997499e956f481b2fb6
Leonardo Campos <leocampos.995@gmail.com>
2023-08-14 13:15:26 -0300

    feat: criando secao 1

--- src/sections/Section1/index.tsx
+++ src/sections/Section1/index.tsx
@@ -1,9 +1,29 @@
+// Styles
 import * as S from "./styles"
+
+// Images
+import ImgMain from "../../assets/Digital.svg"
+
+// Components
+import BtnMain from "../../components/BtnMain"
 
 export default function Section1(){
     return(
         <S.Wrapper>
-            <h1>outra</h1>
+            <S.ImgDesk src={ ImgMain } alt="" />
+            <S.MainContainer>
+                <S.TextMain>
+                    <span>Lorem ipsum </span> dolor sit  amet consectetur adipi consectetur adipiadipiadipi ead
+                </S.TextMain>
+                <S.ImgMobile src={ ImgMain } alt="" />
+                <S.TextSecondary>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </S.TextSecondary>
+                <S.BtnDiv>
+                    <BtnMain bgcolor="#1d8afe" handleClick={() => console.log("clicou")}>saber mais</BtnMain>
+                    <BtnMain bgcolor="#fe731d">comprar</BtnMain>
+                </S.BtnDiv>
+            </S.MainContainer>
         </S.Wrapper>
     )
 }
--- src/sections/Section1/styles.tsx
+++ src/sections/Section1/styles.tsx
@@ -1,9 +1,101 @@
 import styled  from "styled-components";
 
 export const Wrapper = styled.div`
+    position: relative;
     padding: 0 30px;
+    width:100vw;
+    display: flex;
+    justify-content: center;
+    
+    @media (min-width: 913px) {
+        margin-top: 80px;
+        gap: 10px;
+    }
+    
 `
 
-export const ImgContainer = styled.div`
+export const MainContainer = styled.div`
+     display: flex;
+     width: 100%;
+     flex-direction: column;
+     gap: 25px;
+     align-items: center;
+     justify-content: start;
+     position: relative; 
+     padding: 50px 0;
+
+     @media (min-width: 913px) {
+        margin-top: 150px;
+        width: 50%;
+        height: 65vh;
+    
+     }
 
 `
+
+export const TextMain = styled.h1`
+    width: 100%;
+    font-size: ${props => props.theme.text_2xl};
+    text-align: center;
+
+    & span {
+        color: #32abfb;
+    }
+
+    @media (min-width: 913px) {
+        text-align: start;
+        width: 100%;
+        font-size: ${props => props.theme.text_4xl};
+
+    }
+
+`
+
+export const ImgDesk = styled.img`
+    position: relative;
+    top: 0px; 
+    max-width: 40%;
+    min-width: 550px;
+    height: 100%;
+    
+    @media (max-width: 913px) {       
+        display: none;
+    }
+`
+
+export const ImgMobile = styled.img`
+    position: relative;
+    max-width: 70%;
+    @media (min-width: 913px){
+        display: none;
+    }
+`
+
+export const TextSecondary = styled.p`
+    font-size: 16px;
+    text-align: center;
+
+    @media (min-width: 913px) {
+        font-size: ${props => props.theme.text_lg};
+        text-align: start;
+        width: 100%;
+    }
+`
+
+export const BtnDiv = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    gap: 10px;
+
+    @media (min-width: 913px) {
+        margin-top: 50px;
+        text-align: start;
+        position: relative;
+        justify-content: start;
+    }
+
+
+`
+
+
