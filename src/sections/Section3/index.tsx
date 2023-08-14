182fe9d76e3a15392f031d5b13acb259a82ffe74
Leonardo Campos <leocampos.995@gmail.com>
2023-08-14 13:16:55 -0300

    feat: criando secao 3

---
+++ src/sections/Section3/index.tsx
@@ -0,0 +1,12 @@
+import * as S from "./styles"
+
+interface ISection3Props {
+
+}
+export default function Section3(props : ISection3Props){
+    return(
+        <S.Wrapper>
+
+        </S.Wrapper>
+    )
+}
