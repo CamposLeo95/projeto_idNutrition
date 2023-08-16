ae177e5a0cf14f62efeea51efa626cd92b2c9cf6
Leonardo Campos <leocampos.995@gmail.com>
2023-08-16 09:41:37 -0300

    feat: criando first card

---
+++ src/components/FirstCard/index.tsx
@@ -0,0 +1,34 @@
+// Styles
+import * as S from "./styles"
+
+//Icons 
+import { IconType } from 'react-icons';
+
+// Interface
+interface FirstCardProps {
+    img: string
+    icon: IconType
+    textImg: string
+    textCard: string
+    textcolor?: boolean
+    link: string
+}
+
+export default function FirstCard({img, icon: Icon, textImg, textCard, textcolor, link} : FirstCardProps){
+    return(
+        <S.CardContainer>
+            <S.ImgCard img={img} textcolor={textcolor ? "true" : "false"}>
+                <S.ImgConteudo>
+                    <Icon/>
+                    <p>{textImg}</p>
+                </S.ImgConteudo>
+                <S.ImgLink href={link} target="_blank">
+                    <p>Veja Mais</p>
+                </S.ImgLink>
+            </S.ImgCard>
+            <S.TextCard>
+                <p>{textCard}</p>
+            </S.TextCard>
+        </S.CardContainer>
+    )
+}
---
+++ src/components/FirstCard/styles.tsx
@@ -0,0 +1,61 @@
+import styled from "styled-components"
+
+type ImgCardProps ={
+    img: string
+    textcolor?: string
+}
+
+export const CardContainer = styled.div`
+    width: 20%;
+    min-width: 290px;
+    gap: 20px;
+    margin: 10px;
+    background-color: #fff;
+`
+export const ImgCard = styled.div <ImgCardProps>`
+    position: relative;
+    width: 100%;
+    height: 350px;
+    border-radius: 15px;
+    padding: 30px;
+    overflow: hidden;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-image: url(${props => props.img});
+    background-repeat: no-repeat;
+    background-size: cover;
+    display: flex;
+    align-items: start;
+    color: ${props => props.textcolor == "true" ? "#fff" : "#000"};
+
+`
+
+export const ImgConteudo = styled.div`
+    font-size: ${props => props.theme.text_lg};
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 10px;
+`
+
+export const ImgLink = styled.a`
+    position: absolute;
+    background: #fff;
+    width: 100px;
+    padding: 8px;
+    left: 0;
+    bottom: 20px;
+    font-size: ${props => props.theme.text_xs};
+    border-radius: 0 8px 8px 0;
+    text-align: center;
+    cursor: pointer;
+    
+`
+
+export const TextCard = styled.div`
+    width: 100%;
+    padding: 5px;
+
+    font-size: ${props => props.theme.text_sm};
+`
