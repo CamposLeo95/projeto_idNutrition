740c6a8c81e443ed680e1f1360b2ac877a878541
Leonardo Campos <leocampos.995@gmail.com>
2023-08-22 11:29:40 -0300

    chore: add endereço vercel

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL: "http://localhost:5173"
+    // baseURL: "http://localhost:5173"
+    baseURL: "https://projeto-id-nutrition-n54z83u2m-camposleo95.vercel.app"
 })
 
 export default api
