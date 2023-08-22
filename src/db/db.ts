bb990abf376be2829ea3f9b1c81442cbbef1f8a1
Leonardo Campos <leocampos.995@gmail.com>
2023-08-22 16:20:43 -0300

    feat: recriando banco de dados

---
+++ src/db/db.ts
@@ -0,0 +1,92 @@
+import campo from "../assets/campo.png"
+import surf from "../assets/surf.png"
+import praia from "../assets/praia.png"
+
+import { BsFillEmojiLaughingFill } from "react-icons/bs"
+
+const DB = [
+        {
+            img: campo,
+            imgText: "Lorem ipsum dolor sit",
+            cardText: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo magni ipsam in ab officia voluptatibus illum",
+            icon: BsFillEmojiLaughingFill,
+            link: "http://google.com",
+            textColor: true
+        },
+        {
+            img: surf,
+            imgText: "Lorem ipsum dolor sit",
+            cardText: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo magni ipsam in ab officia voluptatibus illum",
+            icon: BsFillEmojiLaughingFill,
+            link: "http://google.com",
+            textColor: false
+        },
+        {
+            img: praia,
+            imgText: "Lorem ipsum dolor sit",
+            cardText: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo magni ipsam in ab officia voluptatibus illum",
+            icon: BsFillEmojiLaughingFill,
+            link: "http://google.com",
+            textColor: true
+        },
+        {
+            img: surf,
+            imgText: "Lorem ipsum dolor sit",
+            cardText: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo magni ipsam in ab officia voluptatibus illum",
+            icon: BsFillEmojiLaughingFill,
+            link: "http://google.com",
+            textColor: false
+        },
+        {
+            img: praia,
+            imgText: "Lorem ipsum dolor sit",
+            cardText: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo magni ipsam in ab officia voluptatibus illum",
+            icon: BsFillEmojiLaughingFill,
+            link: "http://google.com",
+            textColor: true
+        },
+        {
+            img: campo,
+            imgText: "Lorem ipsum dolor sit",
+            cardText: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo magni ipsam in ab officia voluptatibus illum",
+            icon: BsFillEmojiLaughingFill,
+            link: "http://google.com",
+            textColor: false
+        },
+        {
+            img: surf,
+            imgText: "Lorem ipsum dolor sit",
+            cardText: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo magni ipsam in ab officia voluptatibus illum",
+            icon: BsFillEmojiLaughingFill,
+            link: "http://google.com",
+            textColor: true
+        },
+        {
+            img: praia,
+            imgText: "Lorem ipsum dolor sit",
+            cardText: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo magni ipsam in ab officia voluptatibus illum",
+            icon: BsFillEmojiLaughingFill,
+            link: "http://google.com",
+            textColor: false
+        },
+
+        {
+            img: campo,
+            imgText: "Lorem ipsum dolor sit",
+            cardText: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo magni ipsam in ab officia voluptatibus illum",
+            icon: BsFillEmojiLaughingFill,
+            link: "http://google.com",
+            textColor: true
+        },
+
+        {
+            img: surf,
+            imgText: "Lorem ipsum dolor sit",
+            cardText: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo magni ipsam in ab officia voluptatibus illum",
+            icon: BsFillEmojiLaughingFill,
+            link: "http://google.com",
+            textColor: true
+        }
+]
+
+export default DB
