d493fbe13e8268469baa3e58852a1fc54659b43a
Leonardo Campos <leocampos.995@gmail.com>
2023-09-11 09:09:50 -0300

    chore: inserindo imagem

--- src/pages/Home/sections/Section2/index.tsx
+++ src/pages/Home/sections/Section2/index.tsx
@@ -1,11 +1,18 @@
 // Styles
 import * as S from "./styles"
 
+import Pessoa from "../../../../assets/pessoaAgachada.png"
+
 export default function Section2(){
     return(
         <S.Wrapper >
-            <S.MainTitle>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias fuga dolorem perferendis !</S.MainTitle>
-            <S.SubTitle >Lorem, ipsum dolor sit amet consectetur adipisicing </S.SubTitle>
+            <S.MainTitle>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit
+            </S.MainTitle>
+            <S.SubTitle >
+                Lorem, ipsum dolor sit amet consectetur adipisicing 
+            </S.SubTitle>
+            <S.ImgPessoa src={Pessoa} />
         </S.Wrapper>
     )
 }
