859bbc51198d7c7e4259a30849569423d6523b8e
Leonardo Campos <leocampos.995@gmail.com>
2023-09-11 09:03:19 -0300

    chore: refatorando firstcard

--- src/components/FirstCard/index.tsx
+++ src/components/FirstCard/index.tsx
@@ -11,6 +11,7 @@ import BtnMain from "../BtnMain"
 interface FirstCardProps {
     img: string
     icon?: IconType
+    title:string
     textImg: string
     textCard: string
     textcolor?: boolean
@@ -18,19 +19,16 @@ interface FirstCardProps {
     button?: boolean
 }
 
-export default function FirstCard({ img, textCard, textcolor } : FirstCardProps){
+export default function FirstCard({ img, title, textCard, textcolor } : FirstCardProps){
     return(
         <S.CardContainer>
             <S.ImgCard img={img} textcolor={textcolor ? "true" : "false"}>
                 <S.ImgConteudo>
-                    {/* {Icon && <Icon/>} */}
-                    {/* <p>{textImg}</p> */}
+
                 </S.ImgConteudo>
-                {/* <S.ImgLink href={link} target="_blank">
-                    <p>Veja Mais</p>
-                </S.ImgLink> */}
             </S.ImgCard>
             <S.TextCard>
+                <h3>{title}</h3>
                 <p>{textCard}</p>
                 {Button ?  <BtnMain bgcolor="#f45">Ver Mais</BtnMain> : null }
             </S.TextCard>
